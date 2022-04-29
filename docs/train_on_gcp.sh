c9ab24d6a0531544bc889b5f21203a587a5c11a1
shink00000 <r82.y252@gmail.com>
2022-04-29 13:25:25 +0900

    [add] sh

---
+++ docs/train_on_gcp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gsutil cp gs://simss-data/cityscapes_ss.zip .
+gsutil cp gs://simss-data/assets/mit_b2.pth ./assets
+unzip cityscapes_ss.zip -d ./data
+rm cityscapes_ss.zip
+
+pip install torchmetrics==0.7.3
+pip install tensorboard==2.8.0
+pip install -e .
